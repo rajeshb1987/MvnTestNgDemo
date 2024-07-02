4f9ac69007d077d0427a9f4cea5c890975a96072
rajesh b <rajeshbommidi@gmail.com>
2024-07-02 10:29:10 +0530

    new sysout10

--- src/test/java/package1/TwoTestTwo.java
+++ src/test/java/package1/TwoTestTwo.java
@@ -46,7 +46,7 @@ public class TwoTestTwo {
 		  driver.get("https://www.amazon.com/");
 		  System.out.println("title:"+driver.getTitle());
 		 System.out.println("title printed**********************");
-		 System.out.println("title printed helloqqqqaaaaaaaaaaq123qq12qa.**********************");
+		 System.out.println("title printed helloqqqqaaaaaaaaaaq123qq12qaq.**********************");
 		  
 		  System.out.println("Page URL:"+driver.getCurrentUrl());
 		  System.out.println("HERE 111111111111222222222222222");
