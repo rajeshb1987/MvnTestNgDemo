aec9ed8e101d54e2235eac62f5028502193adc89
rajesh b <rajeshbommidi@gmail.com>
2024-07-02 07:41:41 +0530

    added groovy file

---
+++ test.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+    
+    
+
+    stages {
+        
+        
+        stage('Get Source Code') {
+            steps {
+                git credentialsId: 'UNPWD', url: 'https://github.com/rajeshb1987/MvnTestNgDemo.git'
+            }
+        }
+        stage('Build code'){
+            steps{
+                bat script: 'mvn clean compile'
+            }
+        }
+        stage('Run test'){
+            steps{
+                bat script:'mvn clean test -Dbrowser=chrome'
+            }
+        }
+        stage('Publish Report'){
+            steps{
+                publishHTML([allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true, reportDir: '', reportFiles: 'target/surefire-reports/emailable*.html', reportName: 'HTML ReportCI', reportTitles: '', useWrapperFileDirectly: true])
+            }
+        }
+    }
+}
